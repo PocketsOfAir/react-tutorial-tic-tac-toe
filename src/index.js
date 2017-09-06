f171eadaf1dd6927fbcd4034c089d0318f58eef9
Tom Cox <tcox2603@gmail.com>
2017-09-06 11:23:00 +1000

    Changed logic to build board recursively

--- src/index.js
+++ src/index.js
@@ -20,24 +20,28 @@ class Board extends React.Component {
     );
   }
 
+  renderLine(i) {
+    var squares = [];
+    for (var j = 0; j < 3; j++) {
+      squares.push(this.renderSquare(i,j));
+    }
+
+    return (
+      <div className="board-row">
+        {squares}
+      </div>
+    )
+  }
+
   render() {
+    let completeBoard = [];
+    for (let i = 0; i < 3; i++) {
+      completeBoard.push(this.renderLine(i));
+    }
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0, 0)}
-          {this.renderSquare(0, 1)}
-          {this.renderSquare(0, 2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(1, 0)}
-          {this.renderSquare(1, 1)}
-          {this.renderSquare(1, 2)}
-      </div>
-        <div className="board-row">
-          {this.renderSquare(2, 0)}
-          {this.renderSquare(2, 1)}
-          {this.renderSquare(2, 2)}
-      </div>
+        {completeBoard}
       </div>
     );
   }
